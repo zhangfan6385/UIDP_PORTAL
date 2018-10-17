e2e4d6570ee9bdc5934467cfadd485143bd33305
zpjune <1084013577@qq.com>
2018-10-17 10:35:17 +0800

    修改样式

--- src/app_src/views/sever/detail.vue
+++ src/app_src/views/sever/detail.vue
@@ -1,8 +1,8 @@
 <template>
-    <el-row type="flex" style="margin-top:20px;">
+    <el-row type="flex" style="margin-top:20px;margin-bottom:17px">
         <el-col :span="2"> </el-col>
         <el-col :span="20">
-            <el-card shadow="never" style="height:100%;margin-bottom:10px">
+            <el-card shadow="never" style="height:100%;">
                  <div style="width:100%;height:35px">
                     <div style="text-align:left;width:200px;float:left;padding-top:16px;">
                     <el-breadcrumb separator="/" style="font-size:14px;">
